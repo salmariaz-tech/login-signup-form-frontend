ff7576ad6aa8f9677243ddbd0cca3493c303613c
SALMA RIAZ <srabbasiabbasi@gmail.com>
2025-09-09 06:58:11 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({
+export default axios.create({
   baseURL: "https://login-signup-form-server-production.up.railway.app",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
-
-export default API;
-
-
-
-
-
-
